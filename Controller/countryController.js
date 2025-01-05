bb79f3ffd6816f77a8d0f874d8ea8396d1e146df
vishnudhrona <vishnuvaliyaveetil92@gmail.com>
2025-01-05 17:25:21 +0530

    add read.md file

--- Controller/countryController.js
+++ Controller/countryController.js
@@ -1,8 +1,22 @@
 const { default: axios } = require("axios");
 
+const CACHE_DURATION = 3600;
+
 const fetchingCountry = async (req, res) => {
     try{
+      const cachedData = await req.redisClient.get('all_countries');
+        
+      if (cachedData) {
+          return res.status(200).json(JSON.parse(cachedData));
+      }
+
         const response = await axios.get('https://restcountries.com/v3.1/all');
+        await req.redisClient.setEx(
+          'all_countries',
+          CACHE_DURATION,
+          JSON.stringify(response.data)
+      );
+
         res.status(200).json(response.data);        
     } catch(error) {
         res.status(500).json({ message: 'Error fetching countries', error });
@@ -13,6 +27,14 @@ const searchingCountry = async (req, res) => {
     try {
         
         const { region, timezone, capital, name } = req.query;
+
+        const cacheKey = `search_${region || ''}_${timezone || ''}_${capital || ''}_${name || ''}`;
+        
+        const cachedResults = await req.redisClient.get(cacheKey);
+        
+        if (cachedResults) {
+            return res.status(200).json(JSON.parse(cachedResults));
+        }
         
         let url = 'https://restcountries.com/v3.1/all';
         const response = await axios.get(url);
@@ -41,6 +63,14 @@ const searchingCountry = async (req, res) => {
             country.capital && country.capital[0].toLowerCase().includes(capital.toString().toLowerCase())
           );
         }
+
+        if (Object.keys(req.query).length > 0) {
+          await req.redisClient.setEx(
+              cacheKey,
+              CACHE_DURATION,
+              JSON.stringify(filteredCountries)
+          );
+      }
     
         res.status(200).json(filteredCountries);
       } catch (error) {
@@ -48,7 +78,17 @@ const searchingCountry = async (req, res) => {
       }
 }
 
+const clearCache = async (req, res) => {
+  try {
+      await req.redisClient.flushAll();
+      res.status(200).json({ message: 'Cache cleared successfully' });
+  } catch (error) {
+      res.status(500).json({ message: 'Error clearing cache', error });
+  }
+}
+
 module.exports = {
     fetchingCountry,
-    searchingCountry
+    searchingCountry,
+    clearCache
 }
--- app.js
+++ app.js
@@ -4,8 +4,26 @@ const cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const { createClient } = require('redis');
+
 
 var indexRouter = require('./routes/index');
+
+const redisClient = createClient({
+  username: 'default',
+  password: 'X06WbL52ikDsS1HNrbbps2f6yqaZDEW0',
+  socket: {
+      host: 'redis-14007.c305.ap-south-1-1.ec2.redns.redis-cloud.com',
+      port: 14007
+  }
+});
+
+redisClient.on('error', err => console.log('Redis Client Error', err));
+redisClient.on('connect', () => console.log('Connected to Redis'));
+
+(async () => {
+  await redisClient.connect();
+})();
 
 const allowedOrigin = "http://localhost:5173"
 
@@ -20,6 +38,11 @@ const corsOptions = {
 
 var app = express();
 
+app.use((req, res, next) => {
+  req.redisClient = redisClient;
+  next();
+});
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
@@ -31,6 +54,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
+process.on('SIGINT', async () => {
+  await redisClient.quit();
+  process.exit(0);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
